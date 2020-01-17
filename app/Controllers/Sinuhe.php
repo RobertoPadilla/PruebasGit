59371f3b1150f9073cb50b403ea9417f4ae17659
sinuher <33103890+sinuher@users.noreply.github.com>
2020-01-17 12:51:37 -0600

    M first Controller

---
+++ app/Controllers/Sinuhe.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class Sinuhe extends BaseController
+{
+	public function index()
+	{
+		return view('sinuhe');
+	}
+
+	//--------------------------------------------------------------------
+
+}
