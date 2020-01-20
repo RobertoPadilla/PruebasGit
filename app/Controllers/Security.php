ccd18c26d58821ea7147b5ec342b690ebd9880ea
Roberto Padilla <basshivan981@gamil.com>
2020-01-20 11:35:01 -0600

    Con este commit compruebo las llaves privadas y publicas

---
+++ app/Controllers/Security.php
@@ -0,0 +1,10 @@
+<?php namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+
+class Security extends BaseController{
+  public function index(){
+    return view('Security');
+  }
+}
